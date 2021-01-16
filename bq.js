0bfa09855c3e2da7427e9d80cbee3e75f69d6bc5
Alexander Yermolenko <i@surfy.one>
2021-01-16 03:26:47 +0300

    Fixed Access To The Document

--- bq.js
+++ bq.js
@@ -1,9 +1,9 @@
 /* BQ Analytics */
-(function(){
+(function(d){
 	let s,k;
-	s = document.getElementsByTagName("script")[0];
-	k = document.createElement("script")
+	s = d.getElementsByTagName("script")[0];
+	k = d.createElement("script")
 	k.async = 1;
 	k.src = "https://hqmode.com/bq.min.js";
 	s.parentNode.insertBefore(k,s);
-})();
+})(document);
