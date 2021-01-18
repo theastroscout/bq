5e071476c072ef1748f42843052c6a633552c63c
Alexander Yermolenko <i@surfy.one>
2021-01-18 14:01:38 +0300

    New version

--- bq.js
+++ bq.js
@@ -4,6 +4,6 @@
 	s = d.getElementsByTagName("script")[0];
 	k = d.createElement("script")
 	k.async = 1;
-	k.src = "https://hqmode.com/bq.min.js";
+	k.src = "https://hqmode.com/bq.min.js?v=1.0.0";
 	s.parentNode.insertBefore(k,s);
 })(document);
